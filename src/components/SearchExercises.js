6093dba91598e3b17295f0e2670730972eb0ba8b
Roberto Guevara <robertoeguevara@gmail.com>
2023-01-10 21:05:18 -0800

    Make api call to search for exercises

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Button, Stack, TextField, Typography } from '@mui/material'
-import { textTransform } from '@mui/system'
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+
+  const handleSearch = async () => {
+    if(search) {
+      const exercisesData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises',
+        exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
+        || exercise.target.toLowerCase().includes(search)
+        || exercise.equipment.toLowerCase().includes(search)
+        || exercise.bodyPart.toLowerCase().includes(search)
+        );
+        
+        setSearch('');
+        setExercises(searchedExercises);
+    }
+  }
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px" >
       <Typography fontWeight={700} sx={{
@@ -27,8 +49,8 @@ const SearchExercises = () => {
             borderRadius: '40px'
           }}
           height="76px"
-          value=""
-          onChange={(e) => {}}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
@@ -43,6 +65,7 @@ const SearchExercises = () => {
             position: "absolute",
             right: '0'
           }}
+          onClick={handleSearch}
         >
           Search
         </Button>
